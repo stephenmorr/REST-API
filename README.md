957e18ffdc61803c89ca8e7fc8fe23028669e0d7
stephenmorr <sorr@cisco.com>
2017-03-16 12:59:12 -0400

    Updated README

--- README.md
+++ README.md
@@ -3,11 +3,15 @@ Python3 script to access REST-API on an ASR1K
 
 Minimal ASR1K/IOS-XE configuration is located in REST-API-ASR1K.txt
 
+REST-API ASR1k configuration guide:
+http://www.cisco.com/c/en/us/td/docs/routers/csr1000/software/configuration/b_CSR1000v_Configuration_Guide/b_CSR1000v_Configuration_Guide_chapter_01101.pdf
+
+
 Dependencies
 pip install requests
 
 Version 1:
- 1. Takes in username/password and IP address of rest API for IOS-XE and returns token-id
+ 1. Takes in username/password and IP address of rest API for IOS-XE (assumes default port of 55443)
  2. Prints out a list of interfaces
 
 
--- REST_Crypto.py
+++ REST_Crypto.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 
 #Python3 script to call a REST-API on an ASR1K
-#version 1.0 03/12/17
+#version 0.315 03/15/17
 #Stephen Orr
 
 import requests
@@ -37,9 +37,9 @@ def getAuth():
 
 
 def getToken(userauth, hostAddr):
-    #construct http post for token
+    #construct http post for token - REST-API needs a token
     #userauth - dictionary of username and password
-    #hostAddr - IP address of API
+    #hostAddr - IP address of REST-API
     requests.packages.urllib3.disable_warnings()
     url = "https://"+hostAddr+":55443/api/v1/auth/token-services"
     headers = {
@@ -62,7 +62,7 @@ def getToken(userauth, hostAddr):
 def getHostname(hostAddr, sessionToken):
     #get the hostname
     #sessionToken - authorization token
-    #hostAddr - IP address of API
+    #hostAddr - IP address of REST-API
     requests.packages.urllib3.disable_warnings()
     url = "https://"+hostAddr+":55443/api/v1//global/host-name"
     headers = {
@@ -86,7 +86,7 @@ def getHostname(hostAddr, sessionToken):
 def getInterfaces(hostAddr, sessionToken):
     #get the devices interfaces
     #sessionToken - authorization token
-    #hostAddr - IP address of API
+    #hostAddr - IP address of REST-API
     #interface_list - a dictionary with key 'items" of an array of dictionaries
     requests.packages.urllib3.disable_warnings()
     url = "https://"+hostAddr+":55443/api/v1/interfaces"
@@ -107,8 +107,55 @@ def getInterfaces(hostAddr, sessionToken):
     print("Interfaces")
     interface = 0
     while interface < len(interface_list['items']):
-        print(interface_list['items'][interface]['if-name'] , "IP Address:" , interface_list['items'][interface]['ip-address'])
+        print(interface_list['items'][interface]['if-name'], "IP Address", interface_list['items'][interface]['ip-address'])
         interface +=1
+
+
+def getIke(hostAddr, sessionToken):
+    #get the Ike policy information
+    #sessionToken - authorization token
+    #hostAddr - IP address of REST-API
+    requests.packages.urllib3.disable_warnings()
+    url = "https://"+hostAddr+":55443/api/v1/vpn-svc/ike/policies"
+    headers = {
+        "content-type": "application/json",
+        "X-AUTH-TOKEN": sessionToken
+    }
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print (err)
+        exit()
+    except requests.exceptions.RequestException as err:
+        print (err)
+        exit()
+    ikeData = response.json()
+    print("Ike Policies")
+    pprint(ikeData['items'])
+
+def getIPSec(hostAddr, sessionToken):
+    #get the IPSec policy information
+    #sessionToken - authorization token
+    #hostAddr - IP address of REST-API
+    requests.packages.urllib3.disable_warnings()
+    url = "https://"+hostAddr+":55443/api/v1/vpn-svc/ipsec/policies"
+    headers = {
+        "content-type": "application/json",
+        "X-AUTH-TOKEN": sessionToken
+    }
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print (err)
+        exit()
+    except requests.exceptions.RequestException as err:
+        print (err)
+        exit()
+    ipsecData = response.json()
+    print("IPSec Policies")
+    pprint(ipsecData['items'])
 
 
 def main():
@@ -118,7 +165,8 @@ def main():
     token = getToken(login, host_ip) #get the token
     hostname = getHostname(host_ip, token)
     getInterfaces(host_ip, token)
-
+    getIke(host_ip, token)
+    getIPSec(host_ip, token)
 
 if __name__ == "__main__":
     main()
